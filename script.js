451dd84fe2fd9b09e8f75939bd055079feb26032
ByOnez <118693852+ByOnez@users.noreply.github.com>
2023-10-31 13:19:24 -0300

    correção na conversão de dólar para real

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const url = "https://v6.exchangerate-api.com/v6/d9489dc4b4199a9fc53ec9a2/latest/BRL"
+const urlUSD = "https://v6.exchangerate-api.com/v6/d9489dc4b4199a9fc53ec9a2/latest/USD"
 
 let dolar, real
 
-fetch(url)
+fetch(urlUSD)
   .then(response => response.json())
   .then(data => {
     if (data.result === "success") {
       const rates = data.conversion_rates
-      dolar = rates.USD
-      real = rates.BRL
-      console.log(`valor dolar ${dolar}`)
-      console.log(`valor real ${real}`)
+      dolarUSD = rates.USD
+      realUSD = rates.BRL
+      console.log(`valor dolar ${dolarUSD}`)
+      console.log(`valor real ${realUSD}`)
     } else {
       console.error("não foi possível obter o valor da moeda")
     }
     let brlValue = document.getElementById("brl-value")
-    brlValue.innerHTML = `${real.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })} Reais`
+    brlValue.innerHTML = `${realUSD.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })} Reais`
     let dolarInfo = document.getElementById("dolar-info")
 
     let dateHour = new Date()
@@ -39,10 +40,27 @@ fetch(url)
     console.error("Erro:", error)
   })
 
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    if (data.result === "success") {
+      const rates = data.conversion_rates
+      dolar = rates.USD
+      real = rates.BRL
+      console.log(`valor dolar ${dolar}`)
+      console.log(`valor real ${real}`)
+    } else {
+      console.error("não foi possível obter o valor da moeda")
+    }
+  })
+  .catch(error => {
+    console.error("Erro:", error)
+  })
+
 document.addEventListener('DOMContentLoaded', function () {
   const dolarInput = document.getElementById("dolarInput")
   dolarInput.addEventListener("keyup", function () {
-    let dolarCalculate = dolarInput.value / real
+    let dolarCalculate = dolarInput.value / realUSD
     console.log(dolarCalculate)
     let a = document.getElementById("teste")
     a.innerHTML = dolarCalculate.toFixed(2)
